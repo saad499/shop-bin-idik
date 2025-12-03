a71737e74a937baad17385fef082b5c816ff33c9
saad499 <saadkammouss@gmail.com>
2025-12-03 21:36:00 +0100

    Create project

---
+++ src/main/java/org/openlan2/shop_bin_idik/ShopBinIdikApplication.java
@@ -0,0 +1,13 @@
+package org.openlan2.shop_bin_idik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBinIdikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopBinIdikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/openlan2/shop_bin_idik/ShopBinIdikApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openlan2.shop_bin_idik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBinIdikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
